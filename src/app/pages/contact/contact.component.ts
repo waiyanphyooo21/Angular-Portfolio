68d17771193ec1dc6ea5c08fc26045fcc7250980
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-09-13 19:20:25 +0630

    contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  phoneNumber = '+95 979 663 5699';
+  email = 'dm@waiyanphyooo.online';
+  sendData(): void {
+    console.log('sends data to its place');
+  }
 }
