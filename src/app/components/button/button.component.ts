e04c7aec03234b6ab584bb74499193cb63e1be7e
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-09-13 15:54:13 +0630

    button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-button',
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
+  @Input() buttonText: string = '';
+  @Input() buttonAction: () => void = () => {};
 
+  constructor(public router: Router) {}
+
+  onButtonClick(): void {
+    this.buttonAction();
+  }
 }
