51ee64b09894884e43c1a6800a2f4d98b53e0b51
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-09-13 18:57:39 +0630

    services

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  styleUrl: './services.component.scss',
 })
 export class ServicesComponent {
+  constructor(public router: Router) {}
 
+  navigate(): void {
+    this.router.navigate(['/contact']);
+  }
 }
